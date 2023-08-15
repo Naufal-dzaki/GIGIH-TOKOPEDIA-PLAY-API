4b76ee132266e16c55c962bb2cbcbdb238a4c022
Naufal-dzaki <muhammadnaufaldzaki261@gmail.com>
2023-08-15 15:47:58 +0700

    configuration to .env

--- src/config.js
+++ src/config.js
@@ -1,19 +1,22 @@
-import mongoose from 'mongoose';
-import development from '../config/development.json' assert { type: "json" };
+import mongoose from "mongoose";
+import development from "../config/development.json" assert { type: "json" };
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const port = () => {
-    return development.PORT || 8080;
-}
+  return process.env.PORT || 8080;
+};
 
-export const connectDB = async() => {
-    try {
-        await mongoose.connect(development.DB_URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log('Connect to DB');
-    } catch (error) {
-        console.error('Error connecting to MongoDB:', error.message);
-        process.exit(1);
-    }
-}
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connect to DB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error.message);
+    process.exit(1);
+  }
+};
