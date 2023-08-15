8b917eb629b83c703e452acf36b6044bfa9d9ef5
Naufal-dzaki <muhammadnaufaldzaki261@gmail.com>
2023-08-15 15:49:38 +0700

    add get specific video by id

--- src/routes/videosRoutes.js
+++ src/routes/videosRoutes.js
@@ -1,8 +1,12 @@
-import express from 'express';
-import { getVideoList } from '../controllers/videosController.js';
+import express from "express";
+import {
+  getVideoList,
+  getVideoDetail,
+} from "../controllers/videosController.js";
 
 const videosRouter = express.Router();
 
-videosRouter.get('/', getVideoList);
+videosRouter.get("/", getVideoList);
+videosRouter.get("/:id", getVideoDetail);
 
 export default videosRouter;
